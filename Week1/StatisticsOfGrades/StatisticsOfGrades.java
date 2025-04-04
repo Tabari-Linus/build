319c7d0f89353262b94e478021102926c661eea7
Mr Lii <leetamparega@gmail.com>
2025-04-04 11:22:29 +0000

    Graph modified to print properly

--- Week1/StatisticsOfGrades/StatisticsOfGrades.java
+++ Week1/StatisticsOfGrades/StatisticsOfGrades.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class StatisticsOfGrades {
 
+    //A method to return the maximum grade and the minimum grade from the list of grades
     public static double[] getMaxMinGrade(double[] grades) {
         double minGrade = grades[0];
         double maxGrade = grades[0];
@@ -17,14 +18,16 @@ public class StatisticsOfGrades {
         return new double[] { maxGrade, minGrade };
     }
 
+    // A generic method that loops over a string array and display the content of the array
     public static void display(String[] values){
-        for(int j=0; j<values.length; j++){
-            System.out.print(values[j]);
+        for (String value : values) {
+            System.out.print(value);
         }
     }
 
+    // A method to print the statistical graph of the grade frequency
     public static void graph(int[] stats) {
-        String[] axisLabel = {"I    0-20   ","I    21-40   ","I    41-60   ","I    61-80   ","I    81-100   "};
+        String[] axisLabel = {"   I    0-20   ","I    21-40   ","I    41-60   ","I    61-80   ","I    81-100   "};
         String[] axisX = {"   +------------","+------------","+------------","+------------","+------------"};
         int maxStat = 0;
         for (int a = 0; a < stats.length; a++) {
@@ -34,18 +37,18 @@ public class StatisticsOfGrades {
         for (int row = maxStat; row >= 1; row--) {
             System.out.print(row + " > ");
             for (int value : stats) {
-                // Calculate how many # symbols to print for this bar
+                
                 if (value >= row) {
-                    System.out.print(" ##########\t");
+                    System.out.print("  ########## ");
                 } else {
-                    System.out.print("           \t");
+                    System.out.print("             ");
                 }
             }
             System.out.println();
         }
 
         display(axisX);
-        System.err.println();
+        System.out.println();
         display(axisLabel);
         }
 
@@ -60,39 +63,48 @@ public class StatisticsOfGrades {
         double scores[] = new double[N];
         int gradeStats[] = new int[5];
 
+
         try {
             for (int i = 0; i < N; i++) {
                 scores[i] = Double.parseDouble(values[i].trim());
                 if (scores[i] < 0 || scores[i] > 100) {
+                    // Instruction to throw exception if values is greater 100 or less than 0
                     throw new IllegalArgumentException("Grade score greater than 100 or less than 0.");
                 }
+
                 sumOfGrades += scores[i];
 
                 if (scores[i] > 80)
-                    gradeStats[0] += 1;
+                    gradeStats[4] += 1;
                 else if (scores[i] > 60)
-                    gradeStats[1] += 1;
+                    gradeStats[3] += 1;
                 else if (scores[i] > 40)
                     gradeStats[2] += 1;
                 else if (scores[i] > 20)
-                    gradeStats[3] += 1;
+                    gradeStats[1] += 1;
                 else
-                    gradeStats[4] += 1;
+                    gradeStats[0] += 1;
             }
-
+            // calculating the average
             averageGrade = sumOfGrades / N;
+
             maxGrade = getMaxMinGrade(scores)[0];
             minGrade = getMaxMinGrade(scores)[1];
 
+            System.out.println("Values:\n");
             System.out.println("The maximum grade in the class is :" + maxGrade);
             System.out.println("The minimum grade in the class is :" + minGrade);
             System.out.println("The average grade in the class is :" + averageGrade);
 
+            // Displaying graph
+            System.out.println("Graph: \n");
             graph(gradeStats);
         } catch (NumberFormatException e) {
             System.out.println("Value is not a number.");
-        } catch (Exception e) {
-            // TODO: handle exception
+        } catch (IllegalArgumentException e) {
+            System.out.println("Grade should be between from 0 to 100");
+        }
+        catch(Exception e){
             System.out.println("An exception occured");
         }
 
